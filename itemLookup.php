4039566218bf1644a1f0e1df363f74c6f58502b7
Sanjeev Bw <sanjeev@busywizzy.com>
2018-04-25 12:18:52 +0530

    itemLookup.php is updated & almost completed

--- itemLookup.php
+++ itemLookup.php
@@ -79,31 +79,13 @@ class AmazonAPI {
         curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 
         $output = curl_exec($ch);
-
+//p("sss",$output);
         curl_close($ch);
 
         $this->xml = simplexml_load_string($output);
         return $this;
     }
 
-    public function item_lookups($id)
-    {
-        $this->operation = "ItemSearch";
-        $this->Keywords = $id;
-
-        $url = $this->build_url();
-        $ch = curl_init();  
-
-        curl_setopt($ch,CURLOPT_URL,$url);
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-
-        $output = curl_exec($ch);
-
-        curl_close($ch);
-
-        $this->xml = simplexml_load_string($output);
-        return $this;
-    }
 
     public function check_for_errors()
     {
@@ -138,8 +120,11 @@ class AmazonAPI {
         if($this->check_for_errors()) return null;
 
         $product = $this->xml->Items->Item;
+				$json = json_encode($product);
+				$arr = json_decode($json, true);
+				//p($arr);
 				//p($product);
-				p($this->xml->Items);
+				//p($this->xml->Items);
         $item = new STDclass;
         $item->detailedPageURL = $product->DetailPageURL;
         $item->link = "https://www.amazon.com/gp/product/".$this->itemID."/?tag=" . $this->amazon_aff_id;
@@ -148,6 +133,70 @@ class AmazonAPI {
         $item->mediumImage = $product->MediumImage->URL;
         $item->largeImage = $product->LargeImage->URL;
 
+				$jsonData = array();
+				$jsonData['Title'] = $arr['ItemAttributes']['Title'];
+
+				if(isset($arr['ItemAttributes']['ListPrice'])) {
+					$jsonData['ListPrice'] = $arr['ItemAttributes']['ListPrice']['FormattedPrice'];
+					$jsonData['CurrencyCode'] = $arr['ItemAttributes']['ListPrice']['CurrencyCode'];
+				}
+				//EditorialReviews
+				$jsonData['SmallImage'] = $arr['SmallImage']['URL'];
+				$jsonData['MediumImage'] = $arr['MediumImage']['URL'];
+				$jsonData['LargeImage'] = $arr['LargeImage']['URL'];
+				$jsonData['ASIN'] = $arr['ASIN'];
+				if(isset($arr['ParentASIN'])) {
+					$jsonData['ParentASIN'] = $arr['ParentASIN'];
+				}
+				$jsonData['DetailPageURL'] = $arr['DetailPageURL'];
+				$jsonData['SalesRank'] = $arr['SalesRank'];
+				$jsonData['link'] = $item->link;
+				if(isset($arr['OfferSummary']['LowestNewPrice'])) {
+					$jsonData['LowestNewPrice'] = $arr['OfferSummary']['LowestNewPrice']['FormattedPrice'];
+				}
+				
+				if(isset($arr['ItemAttributes']['ReleaseDate'])) {
+					$jsonData['ReleaseDate'] = $arr['ItemAttributes']['ReleaseDate'];
+				}
+				
+				$EditorialReview = $arr['EditorialReviews']['EditorialReview'];
+				foreach($EditorialReview as $review) {
+					if(is_array($review)) {
+						if(in_array("Product Description", $review)) {
+							$jsonData['ProductDescription'] = $review['Content'];
+						}
+						continue;
+					} else {
+						if(in_array("Product Description", $EditorialReview)) {
+							$jsonData['ProductDescription'] = $EditorialReview['Content'];
+						}
+					}
+				}
+				/*
+				p(is_array($EditorialReview));
+				if(count($arr['EditorialReviews']) >1) {	
+					foreach($EditorialReview as $review) {
+						if(in_array("Product Description", $review)) {
+							$jsonData['ProductDescription'] = $review['Content'];
+						}
+					}
+				} else {
+					if(in_array("Product Description", $arr['EditorialReviews']['EditorialReview'])) {
+							$jsonData['ProductDescription'] = $arr['EditorialReviews']['EditorialReview']['Content'];
+					}
+				}
+				*/
+				//p($arr['EditorialReviews']['EditorialReview']['Content']);
+				//MediumImage
+//				$jsonData['CurrencyCode'] = $product->ItemAttributes->ListPrice->CurrencyCode;
+				p(json_encode($jsonData),$jsonData, $arr, $arr['OfferSummary']);
+
+
+				$jsonData = array();
+				$jsonData['ListPrice'] = $product->ItemAttributes->ListPrice->FormattedPrice;
+				$jsonData['CurrencyCode'] = $product->ItemAttributes->ListPrice->CurrencyCode;
+				p(json_encode($jsonData));
+			//	p($jsonData, $product->ItemAttributes->ListPrice, $product->OfferSummary->);
         $item->price = $this->get_item_price($product->OfferSummary);
 
         return $item;
@@ -155,10 +204,14 @@ class AmazonAPI {
 
 }
 
-
+if(empty($_GET['ASIN'])) {
+	die("please send ASIN in url");
+}
 $amazon = new AmazonAPI("rashvin-20", "AKIAINK6LYGABIVDCLVQ", "3bzKyuQQpnCcoWfw/dBzne1Rng+YJ+Lh4VP/2yBe");
-$item = $amazon->item_lookup("B06XX29S9Q")->get_item_data();
+//$item = $amazon->item_lookup("B06XX29S9Q")->get_item_data();
+$item = $amazon->item_lookup($_GET['ASIN'])->get_item_data();
 //$item = $amazon->item_lookup("B00ZV9RDKK")->get_item_data();
+//$item = $amazon->item_lookup("B004GVZUUY")->get_item_data();
 p($item);
 //var_dump($item);
 
